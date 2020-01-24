ec07f04b197ae2787f99e89ae9fdb828a7312d34
jens vanderheyden <Jens.Vanderheyden@vub.ac.be>
2020-01-24 09:00:47 +0100

    wip almost there

--- midi/__init__.py
+++ midi/__init__.py
@@ -11,39 +11,49 @@ class Parser:
             str1 = ''
             str2 = ''
 
-            for port in range(self.n_ports):
-                # print('checking' + str(port))
-                if port < 4:
-                    # str1 = str1 + str(int(str(port) in in_port))
-                    str1 = str1 + str(int(port in config.get(in_port)))
-                    # print('ok' + str1)
-                else:
-                    str2 = str2 + str(int(port in config.get(in_port)))
-
-            # apply hex to the reverse of the string ([::-1]) turned into int
-            print('str1:' + str1)
-            print('str2:' + str2)
-            out.append(str(int(str1[::-1], 2)) + str(int(str2[::-1], 2)))
-            # out.append(hex(int(str1[::-1], 2)) + hex(int(str2[::-1], 2)))
+            for binary_string in self.parse_port(config.get(in_port)):
+                out.append(self.bin2hex(binary_string))
+            # for port in range(self.n_ports):
+            #     # print('checking' + str(port))
+            #     if port < 4:
+            #         # str1 = str1 + str(int(str(port) in in_port))
+            #         str1 = str1 + str(int(port in config.get(in_port)))
+            #         # print('ok' + str1)
+            #     else:
+            #         str2 = str2 + str(int(port in config.get(in_port)))
+            #
+            # # apply hex to the reverse of the string ([::-1]) turned into int
+            # print('str1:' + str1)
+            # print('str2:' + str2)
+            # out.append(str(int(str1[::-1], 2)) + str(int(str2[::-1], 2)))
+            # # out.append(hex(int(str1[::-1], 2)) + hex(int(str2[::-1], 2)))
 
         return ' '.join(out)
 
-    def convert(self, bin):
-        # print "0x%x" % int(bin, 2)
-        hex = "0x%x" % int(bin, 2)
+    def bin2hex(self, binstring):
+        return self.strip(self.convert(binstring))
 
-        return hex.upper()
-        # return hex(int(bin, 2))
+    def convert(self, binstring):
+        hexstring = "0x%x" % int(binstring, 2)
+
+        return hexstring.upper()
 
     def strip(self, hex):
         return hex[:1] + hex[-1]
 
     # # WIP
-    # def parse_port(self, input):
-    #     for i in input:
-    #         if i < 4:
-    #             # str1 = str1 + str(int(str(port) in in_port))
-    #             str1 = str1 + str(int(port in config.get(in_port)))
-    #             # print('ok' + str1)
-    #         else:
-    #             str2 = str2 + str(int(port in config.get(in_port)))
+    def parse_port(self, input):
+        str1 = ''
+        str2 = ''
+        port = 1
+
+        while port <= 8:
+
+            if port <= 4:
+                str1 = str1 + str(int(port in input))
+
+            else:
+                str2 = str2 + str(int(port in input))
+            port += 1
+
+        return [str1, str2]
--- test/test_parser.py
+++ test/test_parser.py
@@ -11,23 +11,23 @@ def strip(input):
     return parser.strip(input)
 
 
-# def test_config_2_hex():
-#     config = {
-#         '1': [2],
-#         '2': [2, 4],
-#         '3': [3, 4, 5],
-#         '4': [7],
-#         '5': [2, 3, 4],
-#         '6': [4, 5, 6],
-#         '7': [8],
-#         '8': [1, 8],
-#     }
-#
-#     expected = '02 00 0A 00 0C 01 00 04 0E 00 08 03 00 08 01 08'
-#
-#     midi_convert = midi.Parser()
-#
-#     assert midi_convert.config_2_hex(config) == expected
+def test_config_2_hex():
+    config = {
+        '1': [2],
+        '2': [2, 4],
+        '3': [3, 4, 5],
+        '4': [7],
+        '5': [2, 3, 4],
+        '6': [4, 5, 6],
+        '7': [8],
+        '8': [1, 8],
+    }
+
+    expected = '02 00 0A 00 0C 01 00 04 0E 00 08 03 00 08 01 08'
+
+    midi_convert = midi.Parser()
+
+    assert midi_convert.config_2_hex(config) == expected
 
 
 # def test_output:
@@ -36,17 +36,22 @@ def strip(input):
 def test_binary_to_hex():
     assert convert("0101") == "0X5"
     assert convert("1110") == "0XE"
-    # assert convert("0001") == "0x5"
-    # assert convert("0011") == "0x6"
-    # assert convert("0100") == "0x1"
+
+    parser = midi.Parser()
+    assert parser.bin2hex('0101') == '05'
+    assert parser.bin2hex('1110') == '0E'
 
 
 def test_short_hex():
-    assert strip("0x5") == "05"
-    assert strip("0xe") == "0e"
+    assert strip("0X5") == "05"
+    assert strip("0XE") == "0E"
 
-# #WIP
-# def test_parse_port():
-#    parser = midi.Parser()
-#    assert parser.parse_port([]) == ['0000', '0000']
-#    assert parser.parse_port([1]) == ['1000', '0000']
+
+# WIP
+def test_parse_port():
+    parser = midi.Parser()
+
+    assert parser.parse_port([]) == ['0000', '0000']
+    assert parser.parse_port([1]) == ['1000', '0000']
+    assert parser.parse_port([2, 3, 4, 7]) == ['0111', '0010']
+    assert parser.parse_port([1, 8]) == ['1000', '0001']
